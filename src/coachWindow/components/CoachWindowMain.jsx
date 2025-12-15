437909de5a6cb430e6c9b0784db7339ad400e128
Lucas Figueira <lucasfigueira137@gmail.com>
2025-12-15 17:14:30 -0300

    reverted useEffect at coachwindowmain

--- src/coachWindow/components/CoachWindowMain.jsx
+++ src/coachWindow/components/CoachWindowMain.jsx
@@ -64,7 +64,7 @@ export default function CoachWindowMain() {
 
     useEffect(() => {
         let isResizing = false;
-        console.log('entra al effect the resize', {currentInsight})
+
         const updateWindowSize = () => {
             if (containerRef.current && window.electronAPI && !isResizing) {
                 isResizing = true;
@@ -159,12 +159,10 @@ export default function CoachWindowMain() {
 
     // Auto-trigger showNext when queue has items and nothing is currently displaying
 	useEffect(() => {
-        console.log('coachchchchchc useeffect')
-		if (insightsQueue.length > 0 && !isCueDisplaying) {
-            console.log('pre showNext!')
+		if (insightsQueue.length > 0 && !isCueDisplaying && !currentInsight) {
 			showNext();
 		}
-    }, [insightsQueue, isCueDisplaying, showNext]);
+    }, [insightsQueue, isCueDisplaying, currentInsight, showNext]);
 
     const DropdownComponent = {
         recall: <SelectProspectDropdown isDropdownOpen={isDropdownOpen} setIsDropdownOpen={setIsDropdownOpen} />,
