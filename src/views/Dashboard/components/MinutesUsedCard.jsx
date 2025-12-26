f97658d4311671b8382520993b32c881c121040f
Lucas Figueira <lucasfigueira137@gmail.com>
2025-12-26 10:18:05 -0300

    added fix for minutes used dashboard widget percentage being infinity

--- src/views/Dashboard/components/MinutesUsedCard.jsx
+++ src/views/Dashboard/components/MinutesUsedCard.jsx
@@ -5,6 +5,7 @@ export default function MinutesUsedCard({ accountUsage, isRefetching }) {
     const { usedMinutes = 0, planMinutes = 0 } = accountUsage || {};
 
     const usedPercentage = ((usedMinutes / planMinutes) * 100).toFixed(2);
+    const formattedUsedPercentage = typeof usedPercentage === 'number' ? usedPercentage : 0;
 
     return (
         <InformativeCard
@@ -16,11 +17,11 @@ export default function MinutesUsedCard({ accountUsage, isRefetching }) {
             <div className='card-content-container'>
                 <div>
                     <p className='card-content-lighter-text'>
-                        <span className='card-content-bold-text'>{usedMinutes}</span> / {planMinutes}min ({usedPercentage}%)
+                        <span className='card-content-bold-text'>{usedMinutes}</span> / {planMinutes}min ({formattedUsedPercentage}%)
                     </p>
                 </div>
                 <div className='progress-bar-container'>
-                    <div className='progress-bar-fill' style={{ width: `${usedPercentage ?? 0}%` }}/>
+                    <div className='progress-bar-fill' style={{ width: `${formattedUsedPercentage}%` }}/>
                 </div>
             </div>
         </InformativeCard>
