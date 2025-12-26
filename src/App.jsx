e32d852cc2ad888bf03e929fe251c015d7e63965
Lucas Figueira <lucasfigueira137@gmail.com>
2025-12-26 11:35:28 -0300

    refactored routing structure watching if user has subscription or not and fixed used minutes remaining being NaN

--- src/App.jsx
+++ src/App.jsx
@@ -1,113 +1,19 @@
-import { Routes, Route } from 'react-router-dom';
-
-//VIEWS
-import Login from './views/Login';
-import Dashboard from './views/Dashboard';
-import Subscription from './views/Subscription/index';
-import Admin from './views/Admin';
-import Checkout from './views/Checkout';
-
 //COMPONENTS
-import AuthGuard from './components/AuthGuard';
-import GuestGuard from './components/GuestGuard';
 import InsightPopUpWrapper from './components/InsightPopUpWrapper';
-import Layout from './components/Layout';
 import PermissionsModalContainer from './components/PermissionsModalContainer';
-import StreamingTest from './components/StreamingTest';
 
 //STYLES
 import './styles/App.css';
 import './styles/Dashboard.css';
 import Providers from './Providers';
-import Account from './views/Account';
+import AppRoutes from './AppRoutes';
 
 function App() {
 
   return (
     <Providers>
       <div className='App'>
-        {/* <TopDragBar /> */}
-        <Routes>
-          <Route
-            path="/login"
-            element={
-              <GuestGuard>
-                <Login />
-              </GuestGuard>
-            }
-          />
-          <Route
-            path="/"
-            element={
-              <AuthGuard>
-                <Layout>
-                  <Dashboard />
-                </Layout>
-              </AuthGuard>
-            }
-          />
-          <Route
-            path="/account"
-            element={
-              <AuthGuard>
-                <Layout>
-                  {/* <Account /> */}
-                  <Account />
-                </Layout>
-              </AuthGuard>
-            }
-          />
-          <Route
-            path="/streaming-test"
-            element={
-              <AuthGuard>
-                <Layout>
-                  <StreamingTest />
-                </Layout>
-              </AuthGuard>
-            }
-          />
-		  <Route
-            path="/checkout"
-            element={
-              <AuthGuard>
-                <Layout>
-                  <Checkout />
-                </Layout>
-              </AuthGuard>
-            }
-          />
-          <Route
-            path="/admin"
-            element={
-              <AuthGuard>
-                <Layout>
-                  <Admin />
-                </Layout>
-              </AuthGuard>
-            }
-          />
-           <Route
-            path="/subscription"
-            element={
-              <AuthGuard>
-                <Layout>
-                  <Subscription />
-                </Layout>
-              </AuthGuard>
-            }
-          />
-          <Route
-            path="/checkout/success"
-            element={
-              <AuthGuard>
-                <Layout>
-                  <Checkout />
-                </Layout>
-              </AuthGuard>
-            }
-          />
-        </Routes>
+        <AppRoutes />
         <InsightPopUpWrapper />
         <PermissionsModalContainer />
       </div>
--- src/views/Dashboard/components/MinutesUsedCard.jsx
+++ src/views/Dashboard/components/MinutesUsedCard.jsx
@@ -1,11 +1,12 @@
 import { LuClock } from "react-icons/lu";
 import InformativeCard from "./InformativeCard";
+import { useMemo } from "react";
 
 export default function MinutesUsedCard({ accountUsage, isRefetching }) {
     const { usedMinutes = 0, planMinutes = 0 } = accountUsage || {};
 
-    const usedPercentage = ((usedMinutes / planMinutes) * 100).toFixed(2);
-    const formattedUsedPercentage = typeof usedPercentage === 'number' ? usedPercentage : 0;
+    const usedPercentage = useMemo(() => Number(((usedMinutes / planMinutes) * 100).toFixed(2)), [usedMinutes, planMinutes]);
+    const safeUsedPercentage = useMemo(() => (typeof usedPercentage === 'number' && !isNaN(usedPercentage)) ? usedPercentage : 0, [usedPercentage]);
 
     return (
         <InformativeCard
@@ -17,11 +18,11 @@ export default function MinutesUsedCard({ accountUsage, isRefetching }) {
             <div className='card-content-container'>
                 <div>
                     <p className='card-content-lighter-text'>
-                        <span className='card-content-bold-text'>{usedMinutes}</span> / {planMinutes}min ({formattedUsedPercentage}%)
+                        <span className='card-content-bold-text'>{usedMinutes}</span> / {planMinutes}min ({safeUsedPercentage}%)
                     </p>
                 </div>
                 <div className='progress-bar-container'>
-                    <div className='progress-bar-fill' style={{ width: `${formattedUsedPercentage}%` }}/>
+                    <div className='progress-bar-fill' style={{ width: `${safeUsedPercentage}%` }}/>
                 </div>
             </div>
         </InformativeCard>
