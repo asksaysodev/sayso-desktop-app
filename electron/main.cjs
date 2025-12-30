b35466caede657fcb618d86dd1b3b8bc18b68bbc
Lucas Figueira <lucasfigueira137@gmail.com>
2025-12-30 16:13:50 -0300

    added setup for electron auto updater

--- electron/main.cjs
+++ electron/main.cjs
@@ -3,6 +3,13 @@ const path = require('node:path');
 const fs = require('node:fs');
 const { WindowManager } = require('./utils/windowManager');
 const { nativeImage } = require('electron/common');
+const { updateElectronApp } = require('update-electron-app');
+
+updateElectronApp({
+  updateInterval: '1 hour',
+  logger: require('electron-log'),
+  notifyUser: true
+});
 
 // Native audio module - will be loaded after logging is set up
 let nativeAudio = null;
