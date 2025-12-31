b46ebf3796d15670a3bdd1954a0bfb951acc68f5
Lucas Figueira <lucasfigueira137@gmail.com>
2025-12-31 18:14:06 -0300

    hid Context Files and Connections and changed tab selector styles

--- src/views/Account/components/AccountSettingsTabs.jsx
+++ src/views/Account/components/AccountSettingsTabs.jsx
@@ -1,24 +1,47 @@
 import { LuUser, LuFileScan, LuBuilding, LuUnplug } from 'react-icons/lu';
 
+const ICON_SIZE = 18;
+
 export default function AccountSettingsTabs({ onSelectPanel, selectedPanel }) {
-  return (
-    <div className='account-settings-selector-container' data-view={selectedPanel}>
-        <div className={`account-settings-view-toggle-button ${selectedPanel === 'personal' ? 'active' : ''}`} onClick={() => onSelectPanel('personal')}>
-            <LuUser />
-            <p>My Information</p>
+
+    const tabs = [
+        {
+            icon: <LuUser size={ICON_SIZE} />,
+            label: 'My Information',
+            value: 'personal'
+        },
+        
+        {
+            icon: <LuBuilding size={ICON_SIZE} />,
+            label: 'My Company',
+            value: 'company'
+        },
+        {
+            icon: <LuFileScan size={ICON_SIZE} />,
+            label: 'Context Files',
+            value: 'files',
+            hidden: true
+        },
+        {
+            icon: <LuUnplug size={ICON_SIZE} />,
+            label: 'Connections',
+            value: 'connections',
+            hidden: true
+        },
+    ];
+    
+    return (
+        <div className='account-settings-selector-container' data-view={selectedPanel}>
+            {tabs.map(({ icon, label, value, hidden = false }) => {
+                if (hidden) return null;
+
+                return (
+                    <div key={value} className={`account-settings-view-toggle-button ${selectedPanel === value ? 'active' : ''}`} onClick={() => onSelectPanel(value)}>
+                        {icon}
+                        <p>{label}</p>
+                    </div>
+                )
+            })}
         </div>
-        <div className={`account-settings-view-toggle-button ${selectedPanel === 'company' ? 'active' : ''}`} onClick={() => onSelectPanel('company')}>
-            <LuBuilding />
-            <p>My Company</p>
-        </div>
-        <div className={`account-settings-view-toggle-button ${selectedPanel === 'files' ? 'active' : ''}`} onClick={() => onSelectPanel('files')}>
-            <LuFileScan />
-            <p>Context Files</p>
-        </div>
-        <div className={`account-settings-view-toggle-button ${selectedPanel === 'connections' ? 'active' : ''}`} onClick={() => onSelectPanel('connections')}>
-            <LuUnplug />
-            <p>Connections</p>
-        </div>
-    </div>
-  )
+    )
 }
