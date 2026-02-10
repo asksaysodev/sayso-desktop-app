4647eecfa5f49f6c2bcbc633652657cbc1d23182
Lucas Figueira <lucasfigueira137@gmail.com>
2026-02-10 14:28:29 -0300

    Added lazy-loading to app routes

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -1,3 +1,4 @@
+import { useMemo, lazy, Suspense } from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 
 import AuthGuard from './components/AuthGuard';
@@ -7,112 +8,115 @@ import Layout from './components/Layout';
 import Login from './views/Login';
 import PasswordRecovery from './views/PasswordRecovery';
 import ResetPassword from './views/ResetPassword';
-import Dashboard from './views/Dashboard';
-import Account from './views/Account';
-import StreamingTest from './components/StreamingTest';
-import Checkout from './views/Checkout';
-import Admin from './views/Admin';
-import Subscription from './views/Subscription';
+
+const Dashboard = lazy(() => import('./views/Dashboard'));
+const Account = lazy(() => import('./views/Account'));
+const StreamingTest = lazy(() => import('./components/StreamingTest'));
+const Checkout = lazy(() => import('./views/Checkout'));
+const Admin = lazy(() => import('./views/Admin'));
+const Subscription = lazy(() => import('./views/Subscription'));
 
 import { useAuth } from './context/AuthContext';
-import { useMemo } from 'react';
+import SaysoLoader from './components/SaysoLoader';
 
 export default function AppRoutes() {
     const { globalUser } = useAuth();
     const hasSubscription = useMemo(() => !!globalUser?.subscription_plan_id, [globalUser]);
     
     return (
-        <Routes>
-            <Route
-                path="/login"
-                element={
-                    <GuestGuard>
-                    <Login />
-                    </GuestGuard>
-                }
-            />
-            <Route
-                path="/forgot-password"
-                element={
-                    <GuestGuard>
-                    <PasswordRecovery />
-                    </GuestGuard>
-                }
-            />
-            <Route
-                path="/reset-password"
-                element={<ResetPassword />}
-            />
-            <Route
-                path="/"
-                element={
-                    <AuthGuard>
-                    <Layout>
-                        {hasSubscription ? <Dashboard /> : <Navigate to="/subscription" replace />}
-                    </Layout>
-                    </AuthGuard>
-                }
-            />
-            <Route
-                path="/account"
-                element={
-                    <AuthGuard>
-                    <Layout>
-                        <Account />
-                    </Layout>
-                    </AuthGuard>
-                }
-            />
-            <Route
-                path="/streaming-test"
-                element={
-                    <AuthGuard>
-                    <Layout>
-                        <StreamingTest />
-                    </Layout>
-                    </AuthGuard>
-                }
-            />
-            <Route
-                path="/checkout"
-                element={
-                    <AuthGuard>
-                    <Layout>
-                        <Checkout />
-                    </Layout>
-                    </AuthGuard>
-                }
-            />
-            <Route
-                path="/admin"
-                element={
-                    <AuthGuard>
-                    <Layout>
-                        <Admin />
-                    </Layout>
-                    </AuthGuard>
-                }
-            />
-            <Route
-                path="/subscription"
-                element={
-                    <AuthGuard>
-                    <Layout>
-                        <Subscription />
-                    </Layout>
-                    </AuthGuard>
-                }
-            />
-            <Route
-                path="/checkout/success"
-                element={
-                    <AuthGuard>
-                    <Layout>
-                        <Checkout />
-                    </Layout>
-                    </AuthGuard>
-                }
-            />
-        </Routes>
+        <Suspense fallback={<SaysoLoader />}>
+            <Routes>
+                <Route
+                    path="/login"
+                    element={
+                        <GuestGuard>
+                        <Login />
+                        </GuestGuard>
+                    }
+                />
+                <Route
+                    path="/forgot-password"
+                    element={
+                        <GuestGuard>
+                        <PasswordRecovery />
+                        </GuestGuard>
+                    }
+                />
+                <Route
+                    path="/reset-password"
+                    element={<ResetPassword />}
+                />
+                <Route
+                    path="/"
+                    element={
+                        <AuthGuard>
+                        <Layout>
+                            {hasSubscription ? <Dashboard /> : <Navigate to="/subscription" replace />}
+                        </Layout>
+                        </AuthGuard>
+                    }
+                />
+                <Route
+                    path="/account"
+                    element={
+                        <AuthGuard>
+                        <Layout>
+                            <Account />
+                        </Layout>
+                        </AuthGuard>
+                    }
+                />
+                <Route
+                    path="/streaming-test"
+                    element={
+                        <AuthGuard>
+                        <Layout>
+                            <StreamingTest />
+                        </Layout>
+                        </AuthGuard>
+                    }
+                />
+                <Route
+                    path="/checkout"
+                    element={
+                        <AuthGuard>
+                        <Layout>
+                            <Checkout />
+                        </Layout>
+                        </AuthGuard>
+                    }
+                />
+                <Route
+                    path="/admin"
+                    element={
+                        <AuthGuard>
+                        <Layout>
+                            <Admin />
+                        </Layout>
+                        </AuthGuard>
+                    }
+                />
+                <Route
+                    path="/subscription"
+                    element={
+                        <AuthGuard>
+                        <Layout>
+                            <Subscription />
+                        </Layout>
+                        </AuthGuard>
+                    }
+                />
+                <Route
+                    path="/checkout/success"
+                    element={
+                        <AuthGuard>
+                        <Layout>
+                            <Checkout />
+                        </Layout>
+                        </AuthGuard>
+                    }
+                />
+            </Routes>
+        </Suspense>
     )
 }
